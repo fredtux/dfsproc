73a39a5127a61d5ff71d29f3de6b9a72485f6aad
Anca <ancuta2603@gmail.com>
2023-01-20 10:17:22 +0200

    Update a1.c

--- anca/a1.c
+++ anca/a1.c
@@ -46,27 +46,36 @@ struct ProcStruct {
 	char name[32];
 };
 
+// the number of our sys call
 #define __NR_dfs 451
+
 
 int main(int argc, char *argv[])
 {
-    struct ProcStruct *ps = malloc(sizeof(struct ProcStruct) * 64);
+	// we allocate our array of processes
+    struct ProcStruct *ps = malloc(sizeof(struct ProcStruct) * 128);
 	
-    pid_t pid = 1;
+    pid_t pid = 1; // we start from the first process
     if(argc > 1)
-        pid = atoi(argv[1]);
+        pid = atoi(argv[1]); // argv[1]- no 1 process
     
     int tmp = 1;
     int32_t *pidc = &tmp;
-    long levels = syscall(__NR_dfs, pid, 64, pidc, ps);
+	// __NR_dfs our syscall code returning levels var value (how many levels has)
+    long levels = syscall(__NR_dfs, pid, 128, pidc, ps); 
     if(levels < 0){
         perror("DFSProc Syscall Failed");
     } else {
         printf("Total number of levels: %ld\n", levels);
     }
-
+// for each process we print the data about each process
 	for(int i = 0; i < *pidc; ++i){
-		// De adaugat printf
+		printf("|");
+		// no of "-" representing the level 
+		for(int j = 0; j < ps[i].level; ++j){
+			printf("-");
+		}
+		printf("%d (%d)\n", ps[i].procID, ps[i].state);
 	}
 
     return 0;
