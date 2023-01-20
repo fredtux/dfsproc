60370d4541960de1af2218cdae0b8dcdd6566d34
Anca <ancuta2603@gmail.com>
2023-01-20 10:18:07 +0200

    Update dfsproc.c

--- dfsproc/dfsproc.c
+++ dfsproc/dfsproc.c
@@ -1,3 +1,12 @@
+/**
+ * error codes:
+ * -1: when the number of proceses read is 0
+ * -2: when no pid counter was read from user space into kernel space
+ * -3: when no bytes of data were copied from kernel space to user space, within the number of processes
+ * -4: when no bytes of data were copied from kernel space to user space regarding process info struct
+ * -5: error in dfs
+ * -6: when the allocation didn't happen
+*/
 #include <linux/kernel.h>
 #include <linux/syscalls.h>
 #include <linux/sched.h>
@@ -8,26 +17,31 @@ int files_to_str(char result[32][32], int32_t *size_files,
 		 struct files_struct *files)
 {
 	char *str = kmalloc(256, GFP_KERNEL);
+
+	// each process has a number of files associated with
+	// files_fdtable returns a pointer to the fdtable, taking as input the files_struct.
+	// once we have fdtable we can iterate over the fdt array, printing the file path of each elem from fdt
 	struct fdtable *fdt = files_fdtable(files);
 
 	(*size_files) = 0;
 
-
+// if we can't allocate memory to our string
 	if (!str)
 		return -1;
-	
 
+// we initialize str
 	memset(str, 0, 32);
 
-
+// we iterate through the files till the max files that files_fdtable has within the limit of 32 positions
 	for (int i = 0; i < fdt->max_fds && i < 32; ++i) {
 		if (fdt->fd[i]) {
-	
+	// we copy the name of our file in tmp
 			char *tmp = d_path(&fdt->fd[i]->f_path, str, 256);
 			if (tmp) {
 				for (int j = 0; j < strlen(tmp) && j < 32; ++j) {
 					result[(*size_files)][j] = tmp[j];
 				}
+				//we limit to 32 characters through '\0'
 				result[(*size_files)][strlen(tmp) > 31 ? 31 : strlen(tmp)] = '\0';
 				(*size_files)++;
 
@@ -42,6 +56,10 @@ int files_to_str(char result[32][32], int32_t *size_files,
 	return 0;
 }
 
+/*
+* task is in essence de current process
+* show is a flag for printing/or not something process related  
+*/
 int dfs(struct task_struct *task, int32_t *pidc, struct ProcStruct *ps,
 	int32_t level, int32_t show, long *maxLevel)
 {
@@ -51,7 +69,11 @@ int dfs(struct task_struct *task, int32_t *pidc, struct ProcStruct *ps,
 	if (level > (*maxLevel))
 		(*maxLevel) = level;
 	
-
+/*
+* for each attribute from the current process info
+* it fills in our data structure
+* the data attributes is taken from the kernel processes structure and populates our structure 
+*/
 	ps[*pidc].procID = task->pid;
 	ps[*pidc].level = level;
 	ps[*pidc].state = task->__state;
@@ -81,17 +103,19 @@ int dfs(struct task_struct *task, int32_t *pidc, struct ProcStruct *ps,
 	ps[*pidc].nr_dirtied = task->nr_dirtied;
 	ps[*pidc].nr_dirtied_pause = task->nr_dirtied_pause;
 	ps[*pidc].dirty_paused_when = task->dirty_paused_when;
-	
+
+	// it brings all the files in our structure
 	if (files_to_str(ps[*pidc].files, &ps[*pidc].size_files, task->files) <
 	    0)
 		return -1;
 
+// it takes the first 16 chars from the process name to associate the name of the process  with our data structure
 	for (int i = 0; i < 16; ++i) {
 		ps[*pidc].name[i] = task->comm[i];
 	}
 	ps[*pidc].name[16] = '\0';
 
-
+// a debug parameter in order to display some info about the current process: name, id, level
 	if (show) {
 		printk(KERN_INFO "Name: %-16s Process ID: %d Level: %d\n",
 		       ps[*pidc].name, task->pid, level);
@@ -99,6 +123,7 @@ int dfs(struct task_struct *task, int32_t *pidc, struct ProcStruct *ps,
 
 	++(*pidc);
 
+//it loops through a list of processes and takes the children and applies dfs on them
 	list_for_each(list, &task->children) {
 		child = list_entry(list, struct task_struct, sibling);
 
@@ -110,11 +135,27 @@ int dfs(struct task_struct *task, int32_t *pidc, struct ProcStruct *ps,
 	return 0;
 }
 
-SYSCALL_DEFINE4(dfsproc, pid_t, pid, int32_t, numPs, int32_t *, pidc, struct ProcStruct *, ps)
-{
-	struct task_struct *task = find_task_by_vpid(pid);
+/**
+ *  Main 4 stages defined in our syscall:
+ * memory allocation
+ * dfs creation using the struct info of the proceeses
+ * copy made from kernel to user
+ * deallocation of memory with kfree
+ * 
+ * Note:
+ *  everything that has k as prefix is referring to kernel space
+ * 
+ * pid_t-> the process that we start with
+ * numPs-> the limit number of processes that we want print (in case of any memory leaks)
+ * pidc-> the number of processes read for real, numPs is its upper limit
+ * ps-> a vector with our struct info about a process as component (defined in dfsproc.h) 
+*/
 
-	int32_t *kpidc = kmalloc(sizeof(int32_t), GFP_KERNEL);
+SYSCALL_DEFINE4(dfsproc, pid_t, pid, int32_t, numPs, int32_t *, pidc, struct ProcStruct *, ps)
+{//a kernel structure that takes a pid and returns a structure with the respective process
+	struct task_struct *task = find_task_by_vpid(pid); 
+
+	int32_t *kpidc = kmalloc(sizeof(int32_t), GFP_KERNEL);//we allocate memory in the kernel space
 	struct ProcStruct *kps = kmalloc(sizeof(struct ProcStruct) * (numPs > 256 ? 256 : numPs), GFP_KERNEL);
 
 	int32_t show;
@@ -122,7 +163,7 @@ SYSCALL_DEFINE4(dfsproc, pid_t, pid, int32_t, numPs, int32_t *, pidc, struct Pro
 	long ml = 0;
 	maxLevel = &ml;
 
-	if(!kps){
+	if(!kps){  
 		kps = kmalloc(sizeof(struct ProcStruct) * (numPs > 256 ? 64 : numPs / 2), GFP_KERNEL);
 		if(!kps){
 			return -6;
@@ -136,9 +177,11 @@ SYSCALL_DEFINE4(dfsproc, pid_t, pid, int32_t, numPs, int32_t *, pidc, struct Pro
 		return -2;
 	}
 
+// used a counter at this point
 	if ((show = *kpidc))
 		*kpidc = 0;
 
+// in kps it will be all the processes seen within a dfs structure
 	if (dfs(task, kpidc, kps, 0, show, maxLevel) < 0) {
 		printk(KERN_INFO "Error in dfs");
 		return -5;
